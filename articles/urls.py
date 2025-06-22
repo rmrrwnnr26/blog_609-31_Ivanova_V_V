4eaebc64b699368452b39c88a2319191198c01f4
rmrrwnnr <ivanova_viktoria2611@mail.ru>
2025-06-22 20:39:09 +0500

    added new feature for deleting and updating articles

--- articles/urls.py
+++ articles/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.article_list, name='article'),
     path('create', views.article_create, name='create'),
     path('<slug:slug>', views.article_item, name='article_detail'),
+    path('update/<slug:slug>', views.article_update, name='update'),
+    path('delete/<slug:slug>', views.article_delete, name='delete'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from articles.models import Article
 
@@ -16,12 +17,41 @@ def article_item(request, slug):
 @login_required(login_url='accounts:login')
 def article_create(request):
     if request.method == 'POST':
-        form = forms.CreateArticle(request.POST, request.FILES)
+        form = forms.ArticleForm(request.POST, request.FILES)
         if form.is_valid():
             instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
             return redirect('homepage')
     else:
-        form = forms.CreateArticle()
-    return render(request, 'articles/article_create.html', {'form': form})
+        form = forms.ArticleForm()
+    return render(request, 'articles/article_form.html', {'form': form})
+
+#TODO: пофиксить повторное сохранение записи
+@login_required(login_url='accounts:login')
+def article_update(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.author.id:
+        if request.method == "POST":
+            form = forms.ArticleForm(request.POST, request.FILES, instance=article)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.author = request.user
+                instance.save()
+                return redirect('articles:article_detail', slug=article.slug)
+        else:
+            form = forms.ArticleForm(instance=article)
+        return render(request, 'articles/article_form.html', {'form': form})
+    return HttpResponse('401 Unauthorized', status=401)
+
+
+@login_required(login_url='accounts:login')
+def article_delete(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.author.id:
+        if request.method == "POST":
+            article.delete()
+            return redirect('homepage')
+        return render(request, 'articles/article_confirm_delete.html', {'article': article})
+    return HttpResponse('401 Unauthorized', status=401)
+
